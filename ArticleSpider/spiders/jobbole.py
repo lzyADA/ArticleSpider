e0e388496f198a237e626fd7e0c91bf59d9439d3
liyang1 <liyang1@zhuge.com>
2020-03-12 18:38:56 +0800

    01

---
+++ ArticleSpider/spiders/jobbole.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class JobboleSpider(scrapy.Spider):
+    name = 'jobbole'
+    allowed_domains = ['news.cnblogs.com']
+    start_urls = ['http://news.cnblogs.com/']
+
+    def parse(self, response):
+        pass
